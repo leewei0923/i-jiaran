d7f64789690f0b893eacfe40a97070303123c781
leewei0923 <2955538482@qq.com>
2022-04-10 12:07:43 +0800

    更改设置

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+/*
+ * @Author: leewei
+ * @Date: 2022-04-10 10:52:32
+ * @LastEditors: leewei
+ * @LastEditTime: 2022-04-10 10:56:06
+ * @FilePath: \i-jiaran\src\index.js
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by leewei, All Rights Reserved. 
+ */
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import * as ReactDOM from "react-dom/client";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
